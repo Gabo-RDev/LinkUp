230de74dee7921050d4a5a3d63f0e24c9af876ae
Wil-JsDev <wilmerdelacruz.dev@gmail.com>
2025-09-13 16:54:06 -0400

    feat: add post category repository implementation

--- BackEnd/LinkUp.Infrastructure.Persistence/Repository/PostCategoryRepository.cs
+++ BackEnd/LinkUp.Infrastructure.Persistence/Repository/PostCategoryRepository.cs
@@ -1,10 +1,24 @@
 using LinkUp.Application.Interfaces.Repository;
+using LinkUp.Application.Pagination;
 using LinkUp.Domain.Models;
 using LinkUp.Infrastructure.Persistence.Context;
+using Microsoft.EntityFrameworkCore;
 
 namespace LinkUp.Infrastructure.Persistence.Repository;
 
-public class PostCategoryRepository(LinkUpDbContext context) : GenericRepository<PostCategory>(context), IPostCategoryRepository
+public class PostCategoryRepository(LinkUpDbContext context)
+    : GenericRepository<PostCategory>(context), IPostCategoryRepository
 {
-    
+    public async Task<PagedResult<PostCategory>> GetPostCategoriesAsync(int page, int pageSize,
+        CancellationToken cancellationToken)
+    {
+        var total = await Context.Set<PostCategory>().CountAsync(cancellationToken);
+
+        var items = await Context.Set<PostCategory>()
+            .Skip((page - 1) * pageSize)
+            .Take(pageSize)
+            .ToListAsync(cancellationToken);
+
+        return new PagedResult<PostCategory>(items, total, page, pageSize);
+    }
 }
